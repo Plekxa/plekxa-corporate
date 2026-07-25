99e087055b67088202a959120e696c9245fbfdc0
Plekxa <info@plekxa.com>
2026-07-25 15:06:48 +0100

    Add files via upload

---
+++ plekxa-corporate/components/BrandMark.tsx
@@ -0,0 +1,1 @@
+export function BrandMark(){return <span className="brand-mark" aria-label="Plekxa"><span className="brand-symbol" aria-hidden="true"><i/><i/></span><span className="brand-word">Plekxa</span></span>}
